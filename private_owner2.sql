832c07e0137c555a2fa62a18089e2bedd33a464d
tlethanawit321 <48791751+tlethanawit321@users.noreply.github.com>
2019-04-28 15:59:29 +0700

    Delete private_owner2.sql

--- private_owner2.sql
+++
@@ -1,8 +0,0 @@
-drop table private_owner;
-create table private_owner(id int primary key auto_increment,
-     owner_no varchar(4) not null,
-     first_name varchar(300) not null,
-     last_name varchar(300) not null,
-     address text,
-     telephone varchar(11) not null
-     );
